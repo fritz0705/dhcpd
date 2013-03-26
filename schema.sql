5f699c221da8b826c2c5b24f2121765e0622e276
Fritz Grimpen <fritz@grimpen.net>
2013-03-26 01:31:14 +0000

    Using case-independent collation to compare MAC addresses

--- schema.sql
+++ schema.sql
@@ -4,11 +4,11 @@
 CREATE TABLE IF NOT EXISTS leases (
 	'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 	'address' STRING NOT NULL,
-	'hwaddr' STRING NOT NULL,
+	'hwaddr' STRING NOT NULL COLLATE NOCASE,
 	'routers' STRING DEFAULT '',
 	'nameservers' STRING DEFAULT '',
 	'prefixlen' INTEGER DEFAULT 24,
 	'leasetime' INTEGER DEFAULT 3600
 );
-CREATE UNIQUE INDEX IF NOT EXISTS leases__ix_hwaddr ON leases ('hwaddr');
+CREATE UNIQUE INDEX IF NOT EXISTS leases__ix_hwaddr ON leases ('hwaddr' COLLATE NOCASE);
 
