338f924ef5c986424959ab4ea47c907b6beec9fc
Fritz Grimpen <fritz@grimpen.net>
2013-03-26 04:41:15 +0000

    Changed database schema to provide more performance and to store more information

--- db.h
+++ db.h
@@ -3,14 +3,14 @@
 static const char DB_SCHEMA[] =
 "CREATE TABLE IF NOT EXISTS leases (\n"
 "	'id' INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,\n"
-"	'address' STRING NOT NULL,\n"
-"	'hwaddr' STRING NOT NULL COLLATE NOCASE,\n"
+"	'address' STRING NOT NULL UNIQUE,\n"
+"	'hwaddr' STRING NOT NULL UNIQUE COLLATE NOCASE,\n"
 "	'routers' STRING DEFAULT '',\n"
 "	'nameservers' STRING DEFAULT '',\n"
 "	'prefixlen' INTEGER DEFAULT 24,\n"
-"	'leasetime' INTEGER DEFAULT 3600\n"
-");\n"
-"CREATE UNIQUE INDEX IF NOT EXISTS leases__ix_hwaddr ON leases ('hwaddr' COLLATE NOCASE);\n";
+"	'leasetime' INTEGER DEFAULT 3600,\n"
+"	'allocated' BOOLEAN DEFAULT 0\n"
+");\n";
 
 #include <sqlite3.h>
 
