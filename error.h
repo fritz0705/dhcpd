f22d6991d3c772d0cd59b461fdea918b10d55279
Fritz Grimpen <fritz@grimpen.net>
2013-04-02 06:02:45 +0000

    Remove usage of temporary buffer in dhcpd_error

--- error.h
+++ error.h
@@ -10,27 +10,13 @@
 
 static inline void dhcpd_error(int _exit, int _errno, const char *fmt, ...)
 {
-	char error[512] = {[0] = 0, [511] = 0};
-
-	char *err = error;
-
 	va_list ap;
 	va_start(ap, fmt);
 
 	if (_errno != 0)
-	{
-#ifndef __STRICT_ANSI__
-		err = stpcpy(err, strerror(_errno));
-		err = stpcpy(err, ": ");
-#else
-		err = err + strlen(strcpy(err, strerror(_errno)));
-		err = err + strlen(strcpy(err, ": "));
-#endif
-	}
+		fprintf(stderr, "%s: ", strerror(_errno));
 
-	vsnprintf(err, (size_t)((error + 512) - err - 1), fmt, ap);
-
-	fputs(error, stderr);
+	vfprintf(stderr, fmt, ap);
 	fputc('\n', stderr);
 
 	if (_exit > 0)
