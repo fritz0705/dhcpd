6b71800644c1c9e696339bfa929c3765f731c466
Fritz Grimpen <fritz@grimpen.net>
2014-01-28 17:21:31 +0000

    Refactor ARRAY_COPY macro

--- src/array.h
+++ src/array.h
@@ -5,11 +5,11 @@
 
 #define ARRAY_COPY(dst, src, len) { \
 		struct { \
-			uint8_t _[len]; \
-		} *_src, *_dst; \
-		_src = (void*)src; \
-		_dst = (void*)dst; \
-		*_dst = *_src; \
+			uint8_t _[(len)]; \
+		} *(_src), *(_dst); \
+		(_src) = (void*)(src); \
+		(_dst) = (void*)(dst); \
+		*(_dst) = *(_src); \
 	}
 #define ARRAY_LEN(a) ((sizeof (a)) / (sizeof *(a)))
 
